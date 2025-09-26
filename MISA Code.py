1a5d3bef89e975249556de90164c1f9840c1fdbe
Aman Raj <amanraj12875@gmail.com>
2025-09-26 15:15:58 -0700

    Update and rename SCI_MISA Code.py to MISA Code.py

--- MISA Code.py
+++ MISA Code.py
@@ -1,25 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 31 09:53:14 2025
-
-@author: amanraj
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 31 09:49:20 2025
-
-@author: amanraj
-"""
-
 import os
 import numpy as np
 import pandas as pd
 from scipy.stats import wasserstein_distance
 
 # === STEP 1: Read input from Excel ===
-input_path = r"FSLR 7_Input_Sensitivity Analysis_Maricopa Ele.xlsx"                                    #CHANGE HERE!!!
-df = pd.read_excel(input_path, sheet_name='FSLR 7_TRB_Smog')                  #CHANGE HERE!!!
+input_path = r"Input excel file path"                                    #CHANGE HERE!!!
+df = pd.read_excel(input_path, sheet_name='sheet name')                  #CHANGE HERE!!!
 
 # === STEP 2: Simulate lognormal variables ===
 n = 10000
@@ -29,8 +15,8 @@ input_names = df['INPUT'].tolist()
 input_values = {}
 for _, row in df.iterrows():
     name = row['INPUT']
-    mu1, sigma1 = row['IN_m'], row['IN_sd']
-    mu2, sigma2 = row['Smog_m'], row['Smog_sd']    #CHANGE HERE!!!
+    mu1, sigma1 = row['AM_m'], row['AM_sd']
+    mu2, sigma2 = row['Impact Category_m'], row['Impact Category_sd']    #CHANGE HERE!!!
     samples = np.exp(
         np.random.normal(mu1, sigma1, n) +
         np.random.normal(mu2, sigma2, n)
@@ -39,17 +25,17 @@ for _, row in df.iterrows():
 
 # === STEP 3 & 4: Compute total output Y based on serial number logic ===
 
-# Add inputs from Serial Number 1 to 25 (i.e., rows 0 to 24)
-# Chnage to 23 for FSLR 4 MISA analysis as it does not have heat and elcricuty for secondary frame production
-# Chnage to 24 for FSLR 6 & FSLR 7 MISA analysis as it has heat and elcricuty for secondary frame production
-X_add = np.column_stack([input_values[df.iloc[i]['INPUT']] for i in range(0, 24)])
+# Add inputs from Serial Number 1 to 25 (i.e., rows 0 to 24) (Environmental Burden)
+# Change to 23 for FSLR 4 MISA analysis, as it does not have heat and electricity for secondary frame production
+# Change to 24 for FSLR 6 & FSLR 7 MISA analysis, as it has heat and electricity for secondary frame production
+X_add = np.column_stack([input_values[df.iloc[i]['INPUT']] for i in range(0, 23)])             #CHANGE HERE!!!
 sum_add = X_add.sum(axis=1)
 
-# Subtract inputs from Serial Number 26 to 39 (i.e., rows 24 to 38)
-# Change to 34 for FSLR 4 MISA analysis as it has two less inventory (Frames)
-# Change to 38 for FSLR 6 MISA analysis as it has virgin Al production and electricity required for alloying
-# Change to 37 for FSLR 7 MISA analysis as it has virgin steel production
-X_sub = np.column_stack([input_values[df.iloc[i]['INPUT']] for i in range(24, 37)]) 
+# Subtract inputs from Serial Number 26 to 39 (i.e., rows 24 to 38) (Environmental Benefit)
+# Change to 34 for FSLR 4 MISA analysis as it has two fewer inventory (Frames)
+# Change to 38 for FSLR 6 MISA analysis, as it has virgin Al production and electricity required for alloying
+# Change to 37 for FSLR 7 MISA analysis, as it has virgin steel production
+X_sub = np.column_stack([input_values[df.iloc[i]['INPUT']] for i in range(23, 34)])            #CHANGE HERE!!!
 sum_sub = X_sub.sum(axis=1)
 
 # Total output
@@ -80,12 +66,13 @@ out_df = pd.DataFrame({
 })
 
 # your desired folder:
-output_dir = r"FSLR 7_MISA Indices"                                         #CHANGE HERE!!!
+output_dir = r"Output file path"                                         #CHANGE HERE!!!
 os.makedirs(output_dir, exist_ok=True)                          
 
 # desired filename (with .xlsx extension)
-filename = "Marcopa Ele_FSLR 7_Smog MISA index.xlsx"                                       #CHANGE HERE!!!
+filename = "Output file name.xlsx"                                       #CHANGE HERE!!!
 output_path = os.path.join(output_dir, filename)
 
 out_df.to_excel(output_path, index=False)
+
 print(f"Results written to:\n{output_path}")
